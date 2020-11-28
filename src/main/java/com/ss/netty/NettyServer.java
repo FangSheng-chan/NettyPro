e5e13a9d3a278ae40fedf440172552cd9c5685f7
ss <445317262@qq.com>
2020-11-29 00:16:24 +0800

    5

--- src/main/java/com/ss/netty/NettyServer.java
+++ src/main/java/com/ss/netty/NettyServer.java
@@ -1,10 +1,41 @@
 package com.ss.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.sctp.nio.NioSctpServerChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class NettyServer {
-    public static void main(String[] args) {
-        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup(8);
 
+        try {
+//        创建服务器端的启动对象，配置参数
+            ServerBootstrap bootstrap = new ServerBootstrap();
+//        配置 bootstrap
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)  //使用NioServerSocketChannel 作为服务器的通道实现
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new NettyServerHandler());
+                        }
+                    });
+            System.out.println("....服务器 is ready");
+            ChannelFuture future = bootstrap.bind(6668).sync();
+            future.channel().closeFuture().sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 }
---
+++ src/main/java/com/ss/netty/test.java
@@ -0,0 +1,9 @@
+package com.ss.netty;
+
+import io.netty.util.NettyRuntime;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(NettyRuntime.availableProcessors());
+    }
+}
